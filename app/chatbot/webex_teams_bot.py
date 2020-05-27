0f73315e253efee40e5daaf67a08490a61a6e1ee
Josh VanDeraa <josh.vanderaa@networktocode.com>
2020-05-27 13:15:45 -0500

    Updates JSON format

--- app/chatbot/webex_teams_bot.py
+++ app/chatbot/webex_teams_bot.py
@@ -11,6 +11,25 @@ flask_app = Flask(__name__)
 
 api = WebexTeamsAPI()
 
+# Verify all of the environment variables are defined and available
+ROOM_NAME = os.environ.get("ROOM_NAME")
+WEBEX_BOT_USERNAME = os.environ.get("WEBEX_BOT_USERNAME")
+WEBEX_TEAMS_ACCESS_TOKEN = os.environ.get("WEBEX_TEAMS_ACCESS_TOKEN")
+
+if ROOM_NAME is None:
+    raise ValueError(
+        f"Please set environment variable for room name, 'export ROOM_NAME=<room_name>'"
+    )
+
+if WEBEX_BOT_USERNAME is None:
+    raise ValueError(
+        f"Please set environment variable for room name, 'export WEBEX_BOT_USERNAME=<webex_bot_username>'"
+    )
+
+if WEBEX_TEAMS_ACCESS_TOKEN is None:
+    raise ValueError(
+        f"Please set environment variable for room name, 'export WEBEX_TEAMS_ACCESS_TOKEN=<webex_teams_access_token>'"
+    )
 
 @flask_app.route("/", methods=["GET"])
 def index():
@@ -24,32 +43,26 @@ def camera_listener():
     Listening for camera messages to be sent from the Meraki dashboard, webhook listener
 
     Expects to receive JSON formatted data with the keys of:
-      "state": [absent/present] to represent a vehicle in the parking spot
-      "image": URL of the image to present to the Teams channel
+      "type" (string): [arrival/departure] Represent the arrival/departing
+      "parking_space" (integer): Parking space
+      "time_elapsed" (float): Length of time that the car was parked
     """
     if request.method == "POST":
         # Check for the state of the vehicle
-        if request.json["state"] == "present":
-            content = "Vehicle has arrived"
-        elif request.json["state"] == "absent":
-            content = "Vehicle has departed"
-
-        # Set the name of the room from env variable
-        room_name = os.environ.get("ROOM_NAME")
-
-        if room_name is None:
-            raise ValueError(
-                "Please set environment variable for room name, 'export ROOM_NAME=<room_name>'"
-            )
+        if request.json["type"] == "arrival":
+            content = f"Vehicle has arrived in Parking Space #{request.json['parking_space']}"
+        elif request.json["type"] == "departure":
+            content = f"Vehicle has departed from Parking Space #{request.json['parking_space']}"
 
         # Get the list of all rooms that the bot is joined to
         all_rooms = api.rooms.list()
-        rooms = [room.id for room in all_rooms if room_name in room.title]
 
-        # Send camera picture to the WebEx Teams chat as the bot
-        api.messages.create(
-            roomId=rooms[0], markdown=f"## {content}", files=[request.json["image"]]
-        )
+        for room in all_rooms:
+            if ROOM_NAME in room.title:
+                # Send camera picture to the WebEx Teams chat as the bot
+                api.messages.create(
+                    roomId=room.id, markdown=f"## {content}"
+                )
 
         return "OK"
 
