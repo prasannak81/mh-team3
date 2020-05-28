5626a127020a460e4b33171bea644e9d1e622142
Bryan Culver <me@bryanculver.com>
2020-05-28 09:20:12 -0400

    Bugfix for method type

--- ui/src/common/post_api_typed.ts
+++ ui/src/common/post_api_typed.ts
@@ -3,8 +3,9 @@ const headers = {
   'Accept': 'application/json',
   'Content-Type': 'application/json'
 }
-export default function post_api<T>(url: string, payload: any): Promise<T> {
-  return fetch(url, {method: 'POST', headers: headers, body: JSON.stringify(payload)}).then(resp => {
+type postApiMethodType = 'POST' | 'PUT'
+export default function post_api<T>(url: string, payload: any, methodType: postApiMethodType = 'POST'): Promise<T> {
+  return fetch(url, {method: methodType, headers: headers, body: JSON.stringify(payload)}).then(resp => {
     if (!resp.ok) {
       throw new Error(resp.statusText)
     }
